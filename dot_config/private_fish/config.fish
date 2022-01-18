07070aff4cb607d0be1d0e9e6c7ed5ce31628201
Jeffrey Alan Scudder <me@jas.life>
2022-01-18 01:03:29 -0800

    Update dot_config/private_fish/config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -17,6 +17,7 @@ alias vimdiff 'nvim -d'
 # shortcuts for editing dot files
 alias ev 'chezmoi edit ~/.config/nvim/init.vim'
 alias ef 'chezmoi edit ~/.config/fish/config.fish'
+alias fishcfg 'source ~/.config/fish/config.fish'
 
 # set default editor to nvim
 set -gx EDITOR nvim
