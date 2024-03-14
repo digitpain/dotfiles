be19674b9a15434a2f3bbe2068136e3b8b6f04e3
Jeffrey Alan Scudder <me@jas.life>
2024-03-14 00:15:56 -0700

    .

--- dot_config/emacs.el
+++ dot_config/emacs.el
@@ -3,15 +3,26 @@
 (setq inhibit-startup-screen t) ;; Disable startup message.
 (setq eshell-banner-message "") ;; No eshell banner.
 
-;; (load-theme 'wombat t) ;; Set a dark theme.
+(load-theme 'wombat t) ;; Set a dark theme.
 (setq initial-scratch-message nil) ;; Empty scratch buffer message.
 (global-display-line-numbers-mode) ;; Always show line numbers.
 
+(menu-bar-mode -1) ;; Disable the menu bar.
+
 (when (window-system)
-  (menu-bar-mode -1) ;; Disable the menu bar.
   (tool-bar-mode -1) ;; Disable the tool bar.
   (fringe-mode 0) ;; Disable fringe indicators.
   (scroll-bar-mode -1)) ;; Disable scroll bar.
+
+;; TODO: This should only be on linux.
+(setq interprogram-cut-function
+      (lambda (text &optional push)
+        (with-temp-buffer
+          (insert text)
+          (call-process-region (point-min) (point-max) "wl-copy"))))
+(setq interprogram-paste-function
+      (lambda ()
+        (shell-command-to-string "wl-paste")))
 
 (setq-default line-spacing 0)
 (xterm-mouse-mode 1)
@@ -47,28 +58,43 @@
   (evil-mode 1)
   (setq-default evil-shift-width 2))
 
+(use-package s :ensure t) ;; `dockerfile-mode` depends on `s`.
 (use-package dockerfile-mode :ensure t) ;; Dockerfile support.
-(use-package s :ensure t) ;; Docker depends on `s`.
 (use-package fish-mode :ensure t) ;; Fish shell syntax.
+;; (use-package gptel :ensure t) ;; ChatGPT / LLM support. 
+(use-package chatgpt-shell
+  :ensure t
+  :custom
+  ((chatgpt-shell-openai-key
+    (lambda ()
+      (getenv "OPENAI_API_KEY")))))
+;; ^ Set via `set -Ux OPENAI_API_KEY "your_api_key_here"` in fish shell.
 
 ;; Prettier-js configuration
 (use-package prettier-js
   :hook (js-mode . prettier-js-mode)
   :bind ("C-c p" . prettier-js))
 
-;; Dockerfile mode configuration
-;; (use-package dockerfile-mode
-;;  :mode "Dockerfile\\'")
-
 ;; Add more use-package blocks for other packages as needed
 
 (add-to-list 'auto-mode-alist '("\\.mjs\\'" . js-mode)) ;; Support mjs files.
+
+;; Function to open eshell and run redis-server
+(defun aesthetic ()
+  "Open eshell and run redis-server."
+  (interactive)
+  (eshell)
+  (insert "redis-server")
+  (eshell-send-input))
+
+;; (global-set-key (kbd "C-c r") 'aesthetic) ;; Bind the function to Ctrl+c r.
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(prettier-js evil)))
+ '(package-selected-packages '(gptel prettier-js evil)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
