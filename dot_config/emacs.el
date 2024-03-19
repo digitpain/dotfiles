02a34bef0ea6e2e02dce5c0c5c8257810e1cf55a
Jeffrey Alan Scudder <me@jas.life>
2024-03-18 19:51:09 -0700

    find terminal rendering bug (gruvbox theme)

--- dot_config/emacs.el
+++ dot_config/emacs.el
@@ -3,14 +3,13 @@
 ;; Open emacs maximized and with undecorated window.
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
 (add-to-list 'default-frame-alist '(undecorated . t))
-
-;; Set the internal border width and color
 (add-to-list 'default-frame-alist '(internal-border-width . 6)) ;; Adjust the border width as needed
 
 (setq inhibit-startup-screen t) ;; Disable startup message.
 (setq eshell-banner-message "") ;; No eshell banner.
 
 (setq initial-scratch-message nil) ;; Empty scratch buffer message.
+
 (global-display-line-numbers-mode) ;; Always show line numbers.
 
 (defun disable-line-numbers-in-eshell () ;; Except when in an `eshell`.
@@ -41,13 +40,14 @@
 ;; (setq interprogram-paste-function
 ;;       (lambda ()
 ;;         (shell-command-to-string "wl-paste")))
+;;
 
 (setq-default line-spacing 0)
 (xterm-mouse-mode 1)
 (defun track-mouse (e))
 (setq mouse-sel-mode t)
 (setq ring-bell-function 'ignore) ;; Ignore scroll bell.
-
+;;
 ;; Set-up a better backup directory.
 (defvar my-backup-directory "~/.emacs.d/backups/")
 (unless (file-exists-p my-backup-directory)
@@ -57,21 +57,18 @@
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 2)
 (setq vc-follow-symlinks t)
-
-;; Initialize package sources
+;;
+;;;; Initialize package sources
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
-
-;; Install and configure use-package
+;;
+;;;; Install and configure use-package
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
 (require 'use-package)
 (setq use-package-always-ensure t)
-
-(use-package gruvbox-theme)
-(load-theme 'gruvbox-light-medium t) ;; Set a theme.
 
 (use-package helm ;; Add helm: https://github.com/emacs-helm/helm/wiki#from-melpa  
   :config
@@ -79,24 +76,6 @@
   (setq helm-mode-fuzzy-match t) ;; Optional: Fuzzy match for helm-mode
   (setq helm-ff-fuzzy-matching t) ;; Enable fuzzy matching for file and buffer names
   (helm-mode 1))
-
-;; Evil mode configuration
-(use-package evil
-  :config
-  (evil-mode 1)
-  (setq-default evil-shift-width 2)
-  ;; override C-p in evil mode
-  (dolist (state '(normal insert visual motion emacs))
-    (evil-define-key state 'global (kbd "C-p") 'project-find-file)))
-
-;;(unless (window-system)
-;;  (require 'evil-terminal-cursor-changer)
-;;  (evil-terminal-cursor-changer-activate)) ; Activates the cursor change
-;;(setq evil-insert-state-cursor 'bar)  ; Line cursor for insert state
-(unless (display-graphic-p)
-        (use-package evil-terminal-cursor-changer)
-        (require 'evil-terminal-cursor-changer)
-        (evil-terminal-cursor-changer-activate))
 
 (global-set-key (kbd "M-x") #'helm-M-x)
 (global-set-key (kbd "C-x r b") #'helm-filtered-bookmarks)
@@ -138,9 +117,9 @@
   :bind ("C-c p" . prettier-js))
 
 ;; Use good clipboard system in terminal mode.
-;; (use-package clipetty
-;;  :ensure t
-;;  :hook (after-init . global-clipetty-mode))
+(use-package clipetty
+ :ensure t
+ :hook (after-init . global-clipetty-mode))
 
 ;; Add more use-package blocks for other packages as needed
 
@@ -154,18 +133,36 @@
   (insert "python3 -m http.server 8888")
   (eshell-send-input))
 
+;; Evil mode configuration
+(use-package evil
+  :config
+  (evil-mode 1)
+  (setq-default evil-shift-width 2)
+  ;; override C-p in evil mode
+  (dolist (state '(normal insert visual motion emacs))
+    (evil-define-key state 'global (kbd "C-p") 'project-find-file)))
+
+(unless (display-graphic-p)
+        (use-package evil-terminal-cursor-changer)
+        (require 'evil-terminal-cursor-changer)
+        (evil-terminal-cursor-changer-activate))
+
+;; This package breaks terminal rendering :(
+;; (use-package gruvbox-theme)
+;; (load-theme 'gruvbox-light-medium t) ;; Set a theme.
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("046a2b81d13afddae309930ef85d458c4f5d278a69448e5a5261a5c78598e012" "98ef36d4487bf5e816f89b1b1240d45755ec382c7029302f36ca6626faf44bbd" "871b064b53235facde040f6bdfa28d03d9f4b966d8ce28fb1725313731a2bcc8" default))
- '(package-selected-packages '(gptel prettier-js evil)))
+   '("871b064b53235facde040f6bdfa28d03d9f4b966d8ce28fb1725313731a2bcc8" default))
+ '(package-selected-packages
+   '(helm prettier-js gruvbox-theme fish-mode evil-terminal-cursor-changer evil dockerfile-mode chatgpt-shell)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-(put 'erase-buffer 'disabled nil)
